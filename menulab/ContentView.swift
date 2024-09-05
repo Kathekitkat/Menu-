fde3fb9bf50b4c66e7d1996577cbb4f10e907dff
Katherine <montesbarrerakatherine@gmail.com>
2024-09-05 09:53:31 -0600

    Initial Commit

---
+++ menulab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  menulab
+//
+//  Created by Katherine Montes on 05/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ menulab/menulabApp.swift
@@ -0,0 +1,17 @@
+//
+//  menulabApp.swift
+//  menulab
+//
+//  Created by Katherine Montes on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct menulabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
